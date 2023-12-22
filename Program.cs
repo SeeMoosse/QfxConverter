e76560dab78c35ceed16bca75d2b7c633991976f
Seamus Collins <colli2sc@mail.uc.edu>
2023-12-22 11:53:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QfxConverter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner(args[0]);
+        }
+    }
+
+    public class Runner
+    {
+        private string _fileName;
+
+        private List<string> _fileContents;
+        public Runner(string fileName)
+        {
+            _fileName = fileName;
+            _fileContents = File.ReadAllLines(_fileName).ToList();
+            ConvertToQbo();
+        }
+
+        private void ConvertToQbo()
+        {
+            int userIdLineIndex = -1, bIdLineIndex = -1;
+            Parallel.ForEach(_fileContents, line =>
+            {
+                if(line.Contains("USERID"))
+                    userIdLineIndex = _fileContents.IndexOf(line);
+                if (line.Contains("BID"))
+                    bIdLineIndex = _fileContents.IndexOf(line);
+            });
+            if(userIdLineIndex >= 0)
+                _fileContents.RemoveAt(userIdLineIndex);
+            if (bIdLineIndex >= 0)
+                _fileContents[bIdLineIndex] = _fileContents[bIdLineIndex].Substring(0, 10) + "2430";
+
+            File.WriteAllLines(_fileName.Replace(Path.GetExtension(_fileName), ".qbo"), _fileContents);
+        }
+    }
+}
---
+++ QfxCustomActions/QfxCustomActions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QfxCustomActions
+{
+    [RunInstaller(true)]
+    public partial class QfxCustomActions : Installer
+    {
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            Registry.SetValue("HKEY_CLASSES_ROOT\\SystemFileAssociations\\.qfx\\Shell\\Qbo_Converter", "", "Convert to QBO File");
+            Registry.SetValue("HKEY_CLASSES_ROOT\\SystemFileAssociations\\.qfx\\Shell\\Qbo_Converter\\Command", "", "\"C:\\Program Files (x86)\\QfxConverter\\QfxConverter.exe\" \"%1\"");
+            base.Commit(savedState);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            RegistryKey key = Registry.ClassesRoot.OpenSubKey("SystemFileAssociations").OpenSubKey(".qfx", true);
+            if (key != null)
+            {
+                key.DeleteSubKeyTree("Shell"); 
+            }
+            base.Uninstall(savedState);
+        }
+    }
+}
